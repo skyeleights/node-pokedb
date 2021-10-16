adc5cb9780413c0dafe7d305f14e840c80f5f795
skyeayyad <skyeayyad@gmail.com>
2021-10-16 15:45:43 +0100

    stuff

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 const temp = fs.readFileSync(`${__dirname}/index.html`, "utf-8");
+const card = fs.readFileSync(`${__dirname}/card.html`, "utf-8");
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
   if (pathname === "/") {
     res.writeHead(200, { "Content-type": "text/html" });
-
-    res.end(temp);
+    const fullpage = temp.replace("{%CARD%}", card);
+    res.end(fullpage);
   } else {
-    res.writeHead(404,{"Content-type":"text/html"});
+    res.writeHead(404, { "Content-type": "text/html" });
     res.end("not found");
   }
 });
