483e077f00dee97cdffc39035aba56b47cd51ed9
skyeayyad <skyeayyad@gmail.com>
2021-10-16 16:32:28 +0100

    stuff

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ const url = require("url");
 const temp = fs.readFileSync(`${__dirname}/index.html`, "utf-8");
 const card = fs.readFileSync(`${__dirname}/card.html`, "utf-8");
 const data = [
-  { id: 1, name: "pikachu", type: "lightning " },
-  { id: 2, name: "charmander", type: "fire" },
-  { id: 3, name: "squirtle", type: "water" },
-  { id: 4, name: "bulbasaur", type: "grass" },
+  { id: 0, name: "pikachu", type: "lightning " },
+  { id: 1, name: "charmander", type: "fire" },
+  { id: 2, name: "squirtle", type: "water" },
+  { id: 3, name: "bulbasaur", type: "grass" },
 ];
 const funPut = function (el, card) {
   let output = card.replace(/{%NAME%}/g, el.name);
   output = output.replace(/{%TYPE%}/g, el.type);
+  output = output.replace(/{%ID%}/g, el.id);
   return output;
 };
 const server = http.createServer((req, res) => {
@@ -21,6 +22,10 @@ const server = http.createServer((req, res) => {
     const cards = data.map((el) => funPut(el, card)).join("");
     const cardsInTemp = temp.replace("{%CARD%}", cards);
     res.end(cardsInTemp);
+  } else if (pathname === "/details") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const result = funPut(data[query.id], card);
+    res.end(result);
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
     res.end("not found");
